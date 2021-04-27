9bda63ff4111db0c63cb49a64da7eda2fc6c2651
jayber-dev <79035534+jayber-dev@users.noreply.github.com>
2021-04-27 15:33:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import random
+
+# ---------------------------- FILE CREATION ----------------------------#
+details_file = open("datafile.txt", "w")
+details_file.write("website, user/email, password \n")
+details_file.close()
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def pass_generator():
     password = ""
@@ -19,8 +26,18 @@ def pass_generator():
     pass_entry.insert(0, password)
     
 
+# ---------------------------- SAVE DETAILS------------------------------ #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_to_file():
+    website = web_entry.get()
+    email = email_entry.get()
+    password_data = pass_entry.get()
+
+
+    details_file = open("datafile.txt", "a")
+    details_file.write(f"{website}, {email}, {password_data}  \n")
+    details_file.close()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 from tkinter import *
@@ -31,29 +48,30 @@ logo = PhotoImage(file="logo.png")
 canvas = Canvas(width=250, height=189)
 canvas.create_image(150, 99, image=logo)
 canvas.grid(row=0,column=1)
-# ---------------------------------------- Label ---------------------------------------------
+# ---------------------------------------- Label ----------------------------------------- #
 web_label = Label(text="website",width=5)
 web_label.grid(row=1, column=0)
 
 web_entry = Entry(width=70)
 web_entry.grid(row=1, column=1, columnspan=2)
-# ----------------------------------------- email/username LABEL ------------------------------
+# ----------------------------------------- email/username LABEL ---------------------- #
 email_label = Label(text="e-mail/username")
 email_label.grid(row=2, column=0)
 
 email_entry = Entry(width=70)
 email_entry.grid(row=2, column=1, columnspan=2)
-# ----------------------------------------- password LABEL ------------------------------------
+# ----------------------------------------- password LABEL --------------------------------- # 
 pass_label = Label(text="password")
 pass_label.grid(row=3, column=0)
 
 pass_entry = Entry(window, width= 43)
+pass_entry.insert(0,"insert or generate a password")
 pass_entry.grid(row=3, column=1)
 
 pass_button = Button(text="generate",width=16, command=pass_generator)
 pass_button.grid(row=3, column=2)
 
-add_to_file = Button(text="save password to file", width=59)
+add_to_file = Button(text="save password to file", width=59, command=save_to_file)
 add_to_file.grid(row=4, column=1, columnspan=3)
 
 window.mainloop()
